53f74907fc65f287bb2ebb00a04ba41dac343c81
Danny <1053784@hr.nl>
2023-02-07 19:55:29 +0100

    Feature login and database

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app.secret_key = "ImTheOneAndOnlyLegendaryPatatoFarmer69"
 # This command creates the "<application directory>" path
 # DATABASE_FILE = os.path.join(app.root_path, 'databases', '   .db')
 
-USER_DATABASE_FILE = os.path.join(app.root_path, 'databases', 'user_details.db')
+USER_DATABASE_FILE = os.path.join(app.root_path, 'databases', 'aanwezigheid.db')
 
    
 # dbm = DatabaseModel(DATABASE_FILE)
@@ -32,8 +32,28 @@ dbu = UserDatabaseModel(USER_DATABASE_FILE)
 def index():
     return render_template("index.html")
 
+# Login function with username session and redirect (Danny)
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print(dbu.user_login(username, password))
+        if dbu.user_login(username, password):
+            session['username'] = username
+            
+            return render_template(
+            "admin.html")
+        else:
+            return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
     app.run(host=FLASK_IP, port=FLASK_PORT, debug=FLASK_DEBUG)
 
+#Function to show image (Danny) 
+def ShowImage():    
+ app = Flask(__name__, static_url_path='/static')
+ return app
+
+ShowImage()
